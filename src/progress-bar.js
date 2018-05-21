c1cef3ae72d9dde92f816f635632a1c53c60ae05
Boris <jobsboris27@gmail.com>
2018-05-21 13:32:45 +0300

    init repo

---
+++ src/progress-bar.js
@@ -0,0 +1,13 @@
+/*
+     _ _      _       _
+ ___| (_) ___| | __  (_)___
+/ __| | |/ __| |/ /  | / __|
+\__ \ | | (__|   < _ | \__ \
+|___/_|_|\___|_|\_(_)/ |___/
+                   |__/
+
+ Version: 1.8.1
+  Author: Boris Mukhin
+ Website: http://jobsboris27.github.io/progress-bar
+    Docs: http://jobsboris27.github.io/progress-bar
+ */
